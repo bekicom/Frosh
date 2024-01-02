d1d32f1cde930bdb27672ce7e55731713a026bf8
Ulugbek Mirdadayev <ulugbekmirdadayev1211@gmail.com>
2024-01-02 19:43:43 +0500

    update style.css, intro.js, style.css and constants.js

--- src/pages/home/intro.js
+++ src/pages/home/intro.js
@@ -2,23 +2,42 @@ import React from 'react';
 import { ReactComponent as Filter } from 'assets/svgs/filter.svg';
 import { ReactComponent as Search } from 'assets/svgs/search.svg';
 
+const filters = [
+  { label: 'Sotish', value: 'sell' },
+  { label: 'Turar joy majmuasi', value: 'resident_complex' },
+  { label: 'Ijara', value: 'rent' }
+];
+
 const Intro = () => {
+  const seelectFilter = (
+    <div className="filter">
+      <button type="button">
+        <Filter />
+        <span>Filter</span>
+      </button>
+      <ul className="select-options">
+        {filters.map((item) => (
+          <li key={item.value}>{item.label}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  const searchBar = (
+    <div className="search-bar">
+      {seelectFilter}
+      <input type="text" placeholder="Qidirish" />
+      <button type="submit">
+        <Search />
+      </button>
+    </div>
+  );
+
   return (
     <div className="intro">
       <div className="content">
         <h2 className="title"> {'Keling sizning orzuyingizdagi\nuyni topaylik!'} </h2>
-        <form className="form">
-          <div className="search-bar">
-            <button type="button" className="filter">
-              <Filter />
-              <span>Filter</span>
-            </button>
-            <input type="text" placeholder="Qidirish" />
-            <button type="submit">
-              <Search />
-            </button>
-          </div>
-        </form>
+        <form className="form">{searchBar}</form>
       </div>
     </div>
   );
