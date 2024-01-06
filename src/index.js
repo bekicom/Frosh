3f4aaefa2561b61f6d5de90338c006461fcbe822
Ulugbek Mirdadayev <ulugbekmirdadayev1211@gmail.com>
2024-01-06 20:52:11 +0500

    update 6 files

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer autoClose={300} pauseOnHover={true} position="top-center" />
     </BrowserRouter>
   </React.StrictMode>
 );
