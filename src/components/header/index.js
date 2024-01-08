60b41186b9b298fa0ba7cd9e1fe975e4caeb907c
unknown <you@example.com>
2024-01-08 17:17:49 +0500

    bekicom

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./style.css"
+import { ReactComponent as Bayroq } from '../../assets/svgs/uzbbayroq.svg';
+
 
 const Header = () => {
-  return <div>Header</div>;
+  return <>
+    <header>
+      <div className="h-left">
+        <Link>
+          Frosh
+        </Link>
+        <Link>
+          1 USD  |  12343,48 UZS
+        </Link>
+
+
+      </div>
+      <div className="h-right">
+
+        <select name="" id="">
+          <option value="">
+            <Bayroq /> uz
+          </option>
+        </select>
+        <select name="" id="">
+          <option value="">
+            UZS
+          </option>
+        </select>
+        <button>
+          E’lon joylash +
+        </button>
+      </div>
+
+    </header>
+
+
+  </>;
 };
 
 export default Header;
